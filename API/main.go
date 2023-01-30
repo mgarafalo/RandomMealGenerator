b188c36f077cd1cf294f2aafa8ba4c6d26dabd08
mgarafalo <71238453+mgarafalo@users.noreply.github.com>
2023-01-30 11:41:26 -0500

    Update main.go

--- API/main.go
+++ API/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/mgarafalo/RandomMealGenerator/API/database"
 	"github.com/mgarafalo/RandomMealGenerator/API/router"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Default config
+	app.Use(cors.New())
+
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders:  "*",
+	}))
 
 	database.ConnectToDb()
 
